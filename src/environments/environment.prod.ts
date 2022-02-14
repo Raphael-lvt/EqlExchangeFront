97b70b8d21ac1d1c904a3e1f81221570f5aafc26
Msaddek <basem.msaddek@gmail.com>
2022-02-14 17:31:46 +0100

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiWalletUrl: 'https://1wallet-env.eba-7cp9usvp.eu-west-3.elasticbeanstalk.com/assets-api-rest',
   backEnd: 'http://3legacy-env.eba-3dibhvqm.eu-west-3.elasticbeanstalk.com',
   apiMatchingEngine:  'http://2matchengine-env.eba-8aanag8h.eu-west-3.elasticbeanstalk.com',
-  backLambda: 'https://4lid7fklik.execute-api.eu-west-3.amazonaws.com/test'
+  backLambda: 'https://b6h7j930y7.execute-api.eu-west-3.amazonaws.com/prod'
 };
